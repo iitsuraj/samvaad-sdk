d1a5324aab055f8773db98e3ddefb2f9c08cb5a7
iitsuraj <suraj.saini.phe17@itbhu.ac.in>
2020-09-18 11:50:55 +0530

    feat: ci workflow

---
+++ src/index.spec.ts
@@ -0,0 +1,9 @@
+import HelloWorld from './index';
+
+test('should return Hello', () => {
+  expect(HelloWorld.get()).toBe(true);
+});
+
+// test('should return true given internal link', () => {
+//   expect(isInternalLink('/some-page')).toBe(true)
+// })
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+class HelloWorld {
+  get(): boolean {
+    // console.log('Hello');
+    return true;
+  }
+}
+
+export default new HelloWorld();
